b32ccba5fc30f644f4912a1062763f9ea58948a2
jaluiovilash <ovilashjalui@gmail.com>
2024-07-29 17:59:48 +0530

    added resume and section

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,7 +6,8 @@ from . import views
 urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
-    path('contact/',views.contact,name='contact'),
-    path('blog/',views.blog,name='blog'),
+    path('resume/',views.resume,name='resume'),
     path('services/',views.services,name='services'),
+    path('blog/',views.blog,name='blog'),
+    path('contact/',views.contact,name='contact'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -9,6 +9,9 @@ def home(request):
 def about(request):
     return render(request,'about.html')
 
+def resume(request):
+    return render(request,'resume.html')
+
 def services(request):
     return render(request,'services.html')
 
